3b67b6948faa2f0a120c948f157cbca4bf714d0b
deven367 <masterdeven@gmail.com>
2023-12-11 23:55:41 -0500

    simple loop to count to 10M

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
     println!("Hello, world!");
+
+    let mut x: i32 = 1;
+    // simple loop to count to 10M
+    loop {
+        if x > 1e7 as i32 {
+            break;
+        }
+        x += 1;
+        println!("{}", x)
+    }
 }
